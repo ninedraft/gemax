6ad0bd7c3d94b0d26766b4a4e2fa7c73afcd9b1b
ninedraft <clamores.pro@gmail.com>
2021-03-18 21:59:22 +0300

    feat(server): add some server utils

---
+++ server_utils.go
@@ -0,0 +1,69 @@
+package gemax
+
+import (
+	"io"
+	urlpkg "net/url"
+	"path"
+	"strings"
+
+	"github.com/ninedraft/gemax/status"
+)
+
+// Redirect client to another page.
+// This handler can work with relative
+// If code is status.Success, then generates a small gemini document with a single redirect link.
+// This mechanism can be used as redirect on other protocol pages.
+//
+// Examples:
+//		Redirect(rw, req, "gemini://other.server.com/page", status.Redirect)
+//		Redirect(rw, req, "../root/page", status.PermanentRedirect)
+//		Redirect(rw, req, "https://wikipedia.org", status.Success)
+func Redirect(rw ResponseWriter, req IncomingRequest, target string, code status.Code) {
+	if code == status.Success {
+		rw.WriteStatus(code, MIMEGemtext)
+		_, _ = io.WriteString(rw, "=> "+target+" redirect\r\n")
+		return
+	}
+	const geminiScheme = "gemini://"
+	if strings.HasPrefix(target, geminiScheme) && len(target) > len(geminiScheme) {
+		// skip URL parsing
+		rw.WriteStatus(code, target)
+		return
+	}
+	var url, errParse = urlpkg.Parse(target)
+	if errParse != nil || url.Host != "" || url.Scheme != "" {
+		rw.WriteStatus(code, target)
+		return
+	}
+	// relative path
+	var oldpath = req.URL().Path
+	if oldpath == "" {
+		oldpath = "/"
+	}
+	rw.WriteStatus(status.Redirect, (&urlpkg.URL{
+		Scheme:   req.URL().Scheme,
+		User:     req.URL().User,
+		Host:     req.URL().Host,
+		Path:     path.Join(oldpath, target),
+		RawQuery: req.URL().RawQuery,
+	}).String())
+}
+
+// NotFound serves a not found error.
+func NotFound(rw ResponseWriter, req IncomingRequest) {
+	rw.WriteStatus(status.NotFound, req.URL().String()+" is not found\r\n")
+}
+
+// Query returns request query string.
+// It expects the vanilla gemini "?query_string" format,
+// so if query contains multiple key=value pairs, then returns false.
+func Query(req IncomingRequest) (string, bool) {
+	var query = req.URL().Query()
+	if len(query) != 1 {
+		return "", false
+	}
+	for key := range req.URL().Query() {
+		return key, true
+	}
+	return "", false
+}
