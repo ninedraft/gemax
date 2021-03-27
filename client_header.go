e4422368a1ad7f7ff46567d40be340b5ec77c3e1
ninedraft <clamores.pro@gmail.com>
2021-03-27 19:26:10 +0300

    fix client header parsing

--- client_header.go
+++ client_header.go
@@ -2,8 +2,8 @@ package gemax
 
 import (
 	"errors"
-	"fmt"
 	"io"
+	"strconv"
 	"strings"
 
 	"github.com/ninedraft/gemax/internal/multierr"
@@ -14,10 +14,10 @@ const MaxHeaderSize = 1024
 
 var errInvalidResponse = errors.New("invalid server response")
 
-func ParseResponseHeader(re io.RuneReader) (status.Code, string, error) {
+func ParseResponseHeader(re io.ByteReader) (status.Code, string, error) {
 	var buf strings.Builder
 	for i := 0; i < MaxHeaderSize; i++ {
-		var ru, _, errRune = re.ReadRune()
+		var ru, errRune = re.ReadByte()
 		if errRune != nil {
 			return -1, "", multierr.Combine(errInvalidResponse, errRune)
 		}
@@ -27,14 +27,18 @@ func ParseResponseHeader(re io.RuneReader) (status.Code, string, error) {
 		if i == MaxHeaderSize {
 			return -1, "", errInvalidResponse
 		}
-		buf.WriteRune(ru)
+		buf.WriteByte(ru)
 	}
 	var line = strings.TrimRight(buf.String(), "\r")
-	var code status.Code
-	var meta string
-	var _, errScan = fmt.Sscanf(line, "%d %s", &code, &meta)
-	if errScan != nil {
-		return -1, "", multierr.Combine(errInvalidResponse, errScan)
+
+	const codePrefixSize = 3
+	if len(line) < codePrefixSize {
+		return -1, "", errInvalidResponse
 	}
-	return code, meta, nil
+	var code, errCode = strconv.Atoi(line[:codePrefixSize])
+	if errCode != nil {
+		return -1, "", errInvalidResponse
+	}
+	var meta = line[codePrefixSize:]
+	return status.Code(code), meta, nil
 }
