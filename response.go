ef1627584005543cf2f969dba901bffa62eb39de
ninedraft <clamores.pro@gmail.com>
2021-03-29 22:08:09 +0300

    refactor(server): :recycle: extract bufwriter to a separate package

--- response.go
+++ response.go
@@ -1,14 +1,12 @@
 package gemax
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"io"
 	"sync"
 
-	"github.com/ninedraft/gemax/internal/multierr"
-
+	"github.com/ninedraft/gemax/internal/bufwriter"
 	"github.com/ninedraft/gemax/status"
 )
 
@@ -22,13 +20,14 @@ type responseWriter struct {
 	status        status.Code
 	statusWritten bool
 	isClosed      bool
-	dst           *bufferedWriter
+	*bufwriterAlias
 }
+
+type bufwriterAlias = bufwriter.Writer
 
 func newResponseWriter(wr io.WriteCloser) *responseWriter {
 	return &responseWriter{
-		dst:      newBufferedWriter(wr),
-		isClosed: false,
+		bufwriterAlias: newBufferedWriter(wr),
 	}
 }
 
@@ -39,7 +38,7 @@ func (rw *responseWriter) WriteStatus(code status.Code, meta string) {
 	if code == status.Success && meta == "" {
 		meta = MIMEGemtext
 	}
-	_, _ = fmt.Fprintf(rw.dst, "%d %s\r\n", code, meta)
+	_, _ = fmt.Fprintf(rw.bufwriterAlias, "%d %s\r\n", code, meta)
 	rw.status = code
 	rw.statusWritten = true
 }
@@ -49,15 +48,7 @@ func (rw *responseWriter) Write(data []byte) (int, error) {
 		return 0, io.ErrNoProgress
 	}
 	rw.WriteStatus(status.Success, MIMEGemtext)
-	return rw.dst.Write(data)
-}
-
-func (rw *responseWriter) WriteString(s string) (int, error) {
-	if rw.isClosed {
-		return 0, io.ErrNoProgress
-	}
-	rw.WriteStatus(status.Success, MIMEGemtext)
-	return rw.dst.WriteString(s)
+	return rw.bufwriterAlias.Write(data)
 }
 
 var errAlreadyClosed = errors.New("already closed")
@@ -68,45 +59,27 @@ func (rw *responseWriter) Close() error {
 	}
 	rw.WriteStatus(status.Success, MIMEGemtext)
 	rw.isClosed = true
-	var errClose = rw.dst.Close()
-	putBufferedWriter(rw.dst)
-	rw.dst = nil
+	var errClose = rw.bufwriterAlias.Close()
+	putBufferedWriter(rw.bufwriterAlias)
+	rw.bufwriterAlias = nil
 	return errClose
-}
-
-type bufferedWriter struct {
-	closer io.Closer
-	*bufio.Writer
-}
-
-func (wr *bufferedWriter) Close() error {
-	return multierr.Combine(
-		wr.Writer.Flush(),
-		wr.closer.Close(),
-	)
 }
 
 const writeBufferSize = 4 * 1024
 
 var bufioWriterPool = &sync.Pool{
 	New: func() interface{} {
-		return bufio.NewWriterSize(nil, writeBufferSize)
+		return bufwriter.New(nil, writeBufferSize)
 	},
 }
 
-func newBufferedWriter(wr io.WriteCloser) *bufferedWriter {
-	var bwr = bufioWriterPool.Get().(*bufio.Writer)
+func newBufferedWriter(wr io.WriteCloser) *bufwriter.Writer {
+	var bwr = bufioWriterPool.Get().(*bufwriter.Writer)
 	bwr.Reset(wr)
-	return &bufferedWriter{
-		closer: wr,
-		Writer: bwr,
-	}
+	return bwr
 }
 
-func putBufferedWriter(bwr *bufferedWriter) {
-	var wr = bwr.Writer
-	bwr.Writer = nil
-	bwr.closer = nil
-	wr.Reset(nil)
-	bufioWriterPool.Put(wr)
+func putBufferedWriter(bwr *bufwriter.Writer) {
+	bwr.Reset(nil)
+	bufioWriterPool.Put(bwr)
 }
