25d8d63473fe69f81706ce2554e53b5da6436c7c
merlin <clamores.pro@gmail.com>
2022-07-27 17:04:23 +0300

    addd redirect support for client

--- gemax/client.go
+++ gemax/client.go
@@ -3,6 +3,7 @@ package gemax
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -20,7 +21,30 @@ import (
 type Client struct {
 	MaxResponseSize int64
 	Dial            func(ctx context.Context, host string, cfg *tls.Config) (net.Conn, error)
+	Redirect        func(ctx context.Context, req *urlpkg.URL, prev []RedirectedRequest) error
 	once            sync.Once
+}
+
+var ErrTooManyRedirects = errors.New("too many redirects")
+
+func (client *Client) checkRedirect(ctx context.Context, req *urlpkg.URL, prev []RedirectedRequest) error {
+	if client.Redirect != nil {
+		return client.Redirect(ctx, req, prev)
+	}
+	return defaultRedirect(ctx, req, prev)
+}
+
+func defaultRedirect(_ context.Context, _ *urlpkg.URL, prev []RedirectedRequest) error {
+	const max = 10
+	if len(prev) < max {
+		return nil
+	}
+	return ErrTooManyRedirects
+}
+
+type RedirectedRequest struct {
+	Req      *urlpkg.URL
+	Response *Response
 }
 
 const readerBufSize = 16 << 10
@@ -28,11 +52,36 @@ const readerBufSize = 16 << 10
 // Fetch gemini resource.
 func (client *Client) Fetch(ctx context.Context, url string) (*Response, error) {
 	client.init()
-	var u, errParseURL = urlpkg.Parse(url)
-	if errParseURL != nil {
-		return nil, fmt.Errorf("parsing URL: %w", errParseURL)
+	var redirects []RedirectedRequest
+	for {
+		var u, errParseURL = urlpkg.Parse(url)
+		if errParseURL != nil {
+			return nil, fmt.Errorf("parsing URL: %w", errParseURL)
+		}
+		if err := client.checkRedirect(ctx, u, redirects); err != nil {
+			return nil, fmt.Errorf("redirect: %w", err)
+		}
+		resp, errFetch := client.fetch(ctx, url, u)
+		if errFetch != nil {
+			return resp, errFetch
+		}
+		if !isRedirect(resp.Status) {
+			return resp, nil
+		}
+		resp.Close()
+		redirects = append(redirects, RedirectedRequest{
+			Req:      u,
+			Response: resp,
+		})
+		url = resp.Meta
 	}
+}
 
+func isRedirect(code status.Code) bool {
+	return code == status.Redirect || code == status.RedirectPermanent
+}
+
+func (client *Client) fetch(ctx context.Context, origURL string, u *urlpkg.URL) (*Response, error) {
 	var host = u.Host
 	if strings.LastIndexByte(host, ':') < 0 {
 		host += ":1965"
@@ -51,7 +100,7 @@ func (client *Client) Fetch(ctx context.Context, url string) (*Response, error) 
 	}
 	ctxConnDeadline(ctx, conn)
 
-	var _, errWrite = io.WriteString(conn, url+"\r\n")
+	var _, errWrite = io.WriteString(conn, origURL+"\r\n")
 	if errWrite != nil {
 		return nil, fmt.Errorf("sending request: %w", errWrite)
 	}
