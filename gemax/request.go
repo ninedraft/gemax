45d63867576c68c511f356c3e8c8631abebd0d2a
merlin <clamores.pro@gmail.com>
2023-06-17 13:50:55 +0300

    fix request parsing

--- gemax/request.go
+++ gemax/request.go
@@ -1,15 +1,23 @@
 package gemax
 
 import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"net/url"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
+var requestSuffix = []byte("\n")
+
 // MaxRequestSize is the maximum incoming request size in bytes.
-const MaxRequestSize = 1026
+const MaxRequestSize = int64(1024 + len("\r\n"))
 
 // IncomingRequest describes a server side request object.
 type IncomingRequest interface {
@@ -21,31 +29,68 @@ var (
 	errDotPath = errors.New("dots in path are not permitted")
 )
 
+var ErrBadRequest = errors.New("bad request")
+
 // ParseIncomingRequest constructs an IncomingRequest from bytestream
 // and additional parameters (remote address for now).
 func ParseIncomingRequest(re io.Reader, remoteAddr string) (IncomingRequest, error) {
-	var reader = io.LimitReader(re, MaxRequestSize)
-	var u string
-	var _, errReadRequest = fmt.Fscanf(reader, "%s\r\n", &u)
-	if errReadRequest != nil {
-		return nil, fmt.Errorf("bad request: %w", errReadRequest)
+	var certs []*x509.Certificate
+	if tlsConn, ok := re.(*tls.Conn); ok {
+		certs = slices.Clone(tlsConn.ConnectionState().PeerCertificates)
 	}
-	var parsed, errParse = url.ParseRequestURI(u)
+
+	re = io.LimitReader(re, MaxRequestSize)
+
+	line, errLine := io.ReadAll(re)
+	if errLine != nil {
+		return nil, fmt.Errorf("%w: %w", ErrBadRequest, errLine)
+	}
+
+	if !bytes.HasSuffix(line, requestSuffix) {
+		return nil, ErrBadRequest
+	}
+
+	line = bytes.TrimRight(line, "\r\n")
+
+	parsed, errParse := url.ParseRequestURI(string(line))
 	if errParse != nil {
-		return nil, fmt.Errorf("bad request: %w", errParse)
+		return nil, fmt.Errorf("%w: %w", ErrBadRequest, errParse)
 	}
-	if strings.Contains(parsed.Path, "/..") {
-		return nil, errDotPath
+
+	if !isValidPath(parsed.Path) {
+		return nil, fmt.Errorf("%w: %w", ErrBadRequest, errDotPath)
 	}
+
+	if parsed.Path == "" {
+		parsed.Path = "/"
+	}
+
 	return &incomingRequest{
 		url:        parsed,
 		remoteAddr: remoteAddr,
+		certs:      certs,
 	}, nil
+}
+
+func isValidPath(path string) bool {
+	if path == "." {
+		return false
+	}
+
+	path = strings.TrimPrefix(path, "/")
+	path = strings.TrimSuffix(path, "/")
+
+	if path == "" {
+		return true
+	}
+
+	return fs.ValidPath(path)
 }
 
 type incomingRequest struct {
 	url        *url.URL
 	remoteAddr string
+	certs      []*x509.Certificate
 }
 
 func (req *incomingRequest) URL() *url.URL {
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/ninedraft/gemax
 
-go 1.19
+go 1.20
 
-require golang.org/x/net v0.2.0
+require (
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+	golang.org/x/net v0.11.0
+)
